2ad6b13d72c373d754dc2dc0ef51943559b6836c
CaioXM01 <caioxm01@gmail.com>
2020-01-14 15:42:01 -0300

    Cadastrando devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
+const DevController = require('./controller/DevController');
 
 const routes = Router();
 
-routes.post('/devs', (req, res) => {
-  console.log(req.body);
-  return res.json({message:'Hello EUUU'});
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
 
